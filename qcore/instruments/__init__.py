88b35c4d4c403f3dd29f19d17e308deecd2b825d
kyle <ktnc985@gmail.com>
2026-06-10 16:16:42 +0800

    feat: removes signalcore drivers from list of imports since they can cause issues

--- qcore/instruments/__init__.py
+++ qcore/instruments/__init__.py
@@ -1,6 +1,6 @@
 """ """
 
-from qcore.instruments.drivers import Octave, OPXPlus, GS200, LMS, MS46522B, QM, SA124, SC5503B, SC5511A, APUASYN20
+from qcore.instruments.drivers import Octave, OPXPlus, GS200, LMS, MS46522B, QM, SA124, APUASYN20
 from qcore.instruments.instrument import DummyInstrument
 
 __all__ = [
--- qcore/instruments/drivers/__init__.py
+++ qcore/instruments/drivers/__init__.py
@@ -3,8 +3,8 @@ from qcore.instruments.drivers.qm_octave_dummy import Octave
 from qcore.instruments.drivers.qm_opx import OPXPlus, OPX1000
 from qcore.instruments.drivers.qm import QM
 
-from qcore.instruments.drivers.signalcore_sc5503b import SC5503B
-from qcore.instruments.drivers.signalcore_sc5511a import SC5511A
+# from qcore.instruments.drivers.signalcore_sc5503b import SC5503B
+# from qcore.instruments.drivers.signalcore_sc5511a import SC5511A
 from qcore.instruments.drivers.signalhound_sa124 import SA124
 from qcore.instruments.drivers.vaunix_lms import LMS
 from qcore.instruments.drivers.yokogawa_gs200 import GS200
